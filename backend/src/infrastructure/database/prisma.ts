5a2ecffa28b160338df802ad307cfa12c14f53f0
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-04-29 15:16:10 -0300

    add PrismaService for singleton PrismaClient instance management

---
+++ backend/src/infrastructure/database/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+export class PrismaService extends PrismaClient {
+    private static instance: PrismaClient;
+
+    public static getInstance() {
+        if (!PrismaService.instance) return new PrismaClient();
+        return PrismaService.instance;
+    }
+}
